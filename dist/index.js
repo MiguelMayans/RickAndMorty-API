1004ce54db22ff09d16c0c48bb5c0d3d677b2606
Miguel Mayans <miguelmayansv@gmail.com>
2023-08-09 14:37:57 +0200

    version 0.7

--- dist/index.js
+++ dist/index.js
@@ -16,8 +16,8 @@ function init() {
     return __awaiter(this, void 0, void 0, function* () {
         const episodes = yield getEpisodes(1);
         episodes.forEach((episode) => {
-            const episodeTitle = document.createElement("div");
-            episodeTitle.classList.add("list-group-item", "list-group-item-action", "list-group-item-light", "p-2");
+            const episodeTitle = document.createElement("a");
+            episodeTitle.classList.add("list-group-item", "list-group-item-action", "list-group-item-light", "p-2", "cursor-pointer");
             episodeTitle.textContent = `${episode.episode} - ${episode.name}`;
             episodeList === null || episodeList === void 0 ? void 0 : episodeList.appendChild(episodeTitle);
             episodeTitle.addEventListener(("click"), () => showCharacters(episode));
@@ -51,23 +51,53 @@ function showCharacters(episode) {
                         mainDiv.classList.add("col", "g-4");
                         characterContainer === null || characterContainer === void 0 ? void 0 : characterContainer.appendChild(mainDiv);
                         const cardContainer = document.createElement("div");
-                        cardContainer.classList.add("card", "border-3", "border-dark", "rounded", "h-100");
+                        cardContainer.classList.add("card", "border-3", "border-dark", "rounded", "h-100", "cursor-pointer");
                         mainDiv.appendChild(cardContainer);
                         const cardImage = document.createElement("img");
                         cardImage.classList.add("card-image-top");
                         cardImage.src = character.image;
                         cardImage.alt = `${character.id} image`;
                         cardContainer.appendChild(cardImage);
+                        cardImage.setAttribute("data-bs-toggle", "modal");
+                        cardImage.setAttribute("data-bs-target", "#modal");
                         cardImage.addEventListener("click", showModal);
+                        function showModal() {
+                            return __awaiter(this, void 0, void 0, function* () {
+                                const modal = document.querySelector("#modal");
+                                modal.showModal;
+                                const charName = document.querySelector("#charName");
+                                charName.textContent = character.name;
+                                const charImage = document.querySelector("#charImage");
+                                charImage.src = character.image;
+                                charImage.alt = `${character.id} image`;
+                                const charText = document.querySelector("#charText");
+                                charText.textContent = `Gender: ${character.gender} | Status: ${character.status} | Specie: ${character.species} | Location: ${character.location.name}`;
+                                const charEpisodeList = document.querySelector("#charEpisodeList");
+                                charEpisodeList.textContent = "";
+                                for (let i = 1; i <= 3; i++) {
+                                    const episodes = yield getEpisodes(i);
+                                    episodes.forEach((episode) => {
+                                        const urlChars = episode.characters;
+                                        urlChars.forEach(url => {
+                                            if (character.url == url) {
+                                                const createEpisodes = document.createElement("div");
+                                                createEpisodes.textContent = `${episode.episode} - ${episode.name}`;
+                                                charEpisodeList.appendChild(createEpisodes);
+                                            }
+                                        });
+                                    });
+                                }
+                            });
+                        }
                         const cardBody = document.createElement("div");
                         cardBody.classList.add("card-body");
                         cardContainer.appendChild(cardBody);
                         const cardTitle = document.createElement("h5");
                         cardTitle.classList.add("card-title", "border-bottom", "border-3", "border-dark", "p-1");
                         cardTitle.textContent = character.name;
-                        const cardType = document.createElement("p");
-                        cardType.classList.add("card-text");
-                        cardType.textContent = `Type: ${character.type}`;
+                        const cardSpicie = document.createElement("p");
+                        cardSpicie.classList.add("card-text");
+                        cardSpicie.textContent = `Specie: ${character.species}`;
                         const cardStatus = document.createElement("p");
                         cardStatus.classList.add("card-text");
                         cardStatus.textContent = `Status: ${character.status}`;
@@ -75,7 +105,7 @@ function showCharacters(episode) {
                         cardDimension.classList.add("card-text");
                         cardDimension.textContent = `Dimension: ${character.location.name}`;
                         cardBody.appendChild(cardTitle);
-                        cardBody.appendChild(cardType);
+                        cardBody.appendChild(cardSpicie);
                         cardBody.appendChild(cardStatus);
                         cardBody.appendChild(cardDimension);
                     }
@@ -103,9 +133,5 @@ function showMore() {
             });
         }
     });
-}
-function showModal() {
-    const modal = document.querySelector("#modal");
-    modal.showModal();
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ async function init() {
     const episodes = await getEpisodes(1);
 
     episodes.forEach((episode) => {
-        const episodeTitle = document.createElement("div")
-        episodeTitle.classList.add("list-group-item", "list-group-item-action", "list-group-item-light", "p-2")
+        const episodeTitle = document.createElement("a")
+        episodeTitle.classList.add("list-group-item", "list-group-item-action", "list-group-item-light", "p-2", "cursor-pointer")
         episodeTitle.textContent = `${episode.episode} - ${episode.name}`
         episodeList?.appendChild(episodeTitle)
 
@@ -61,7 +61,7 @@ async function showCharacters(episode: Episodes) {
                     characterContainer?.appendChild(mainDiv)
 
                     const cardContainer = document.createElement("div")
-                    cardContainer.classList.add("card", "border-3", "border-dark", "rounded", "h-100")
+                    cardContainer.classList.add("card", "border-3", "border-dark", "rounded", "h-100", "cursor-pointer")
                     mainDiv.appendChild(cardContainer)
 
                     const cardImage = document.createElement("img")
@@ -69,7 +69,47 @@ async function showCharacters(episode: Episodes) {
                     cardImage.src = character.image
                     cardImage.alt = `${character.id} image`
                     cardContainer.appendChild(cardImage)
+
+                    // Show Modal
+
+                    cardImage.setAttribute("data-bs-toggle", "modal")
+                    cardImage.setAttribute("data-bs-target", "#modal")
                     cardImage.addEventListener("click", showModal)
+
+                    async function showModal() {
+                        const modal = document.querySelector("#modal") as HTMLDialogElement
+                        modal.showModal
+
+                        const charName = document.querySelector("#charName") as HTMLElement
+                        charName.textContent = character.name
+
+                        const charImage = document.querySelector("#charImage") as HTMLImageElement
+                        charImage.src = character.image
+                        charImage.alt = `${character.id} image`
+
+                        const charText = document.querySelector("#charText") as HTMLElement
+                        charText.textContent = `Gender: ${character.gender} | Status: ${character.status} | Specie: ${character.species} | Location: ${character.location.name}`
+
+                        const charEpisodeList = document.querySelector("#charEpisodeList") as HTMLElement
+                        charEpisodeList.textContent = ""
+
+                        for (let i = 1; i <= 3; i++) {
+                            const episodes = await getEpisodes(i)
+
+                            episodes.forEach((episode) => {
+                                const urlChars = episode.characters
+
+                                urlChars.forEach(url => {
+
+                                    if (character.url == url) {
+                                        const createEpisodes = document.createElement("div")
+                                        createEpisodes.textContent = `${episode.episode} - ${episode.name}`
+                                        charEpisodeList.appendChild(createEpisodes)
+                                    }
+                                })
+                            })
+                        }
+                    }
 
                     const cardBody = document.createElement("div")
                     cardBody.classList.add("card-body")
@@ -79,9 +119,9 @@ async function showCharacters(episode: Episodes) {
                     cardTitle.classList.add("card-title", "border-bottom", "border-3", "border-dark", "p-1")
                     cardTitle.textContent = character.name
 
-                    const cardType = document.createElement("p")
-                    cardType.classList.add("card-text")
-                    cardType.textContent = `Type: ${character.type}`
+                    const cardSpicie = document.createElement("p")
+                    cardSpicie.classList.add("card-text")
+                    cardSpicie.textContent = `Specie: ${character.species}`
 
                     const cardStatus = document.createElement("p")
                     cardStatus.classList.add("card-text")
@@ -92,7 +132,7 @@ async function showCharacters(episode: Episodes) {
                     cardDimension.textContent = `Dimension: ${character.location.name}`
 
                     cardBody.appendChild(cardTitle)
-                    cardBody.appendChild(cardType)
+                    cardBody.appendChild(cardSpicie)
                     cardBody.appendChild(cardStatus)
                     cardBody.appendChild(cardDimension)
                 }
@@ -125,12 +165,4 @@ async function showMore() {
         }
         )
     }
-}
-
-// Modal
-
-function showModal() {
-    const modal = document.querySelector("#modal") as HTMLDialogElement
-    modal.showModal
-
 }
